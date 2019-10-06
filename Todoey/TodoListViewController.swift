75c98c5117211ebbb88d8fa553e377075a6c76b8
Piotr Cybulski <piotrcybulski.fm@gmail.com>
2019-10-06 15:57:26 +0200

    Syncing with remote repo + defaults added

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,12 +10,17 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    let todoListArray = ["A", "B", "C", "B", "C", "B", "C", "B", "C", "B", "C", "B", "C", "B", "C", "B", "C", "B", "C"]
+    var todoListArray = ["A", "B", "C"]
+    var defaults = UserDefaults()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tableView.alwaysBounceVertical = false
         // Do any additional setup after loading the view.
+        
+        if let items = defaults.object(forKey: "itemsList") as? [String] {
+            todoListArray = items
+        }
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -37,10 +42,34 @@ class TodoListViewController: UITableViewController {
         } else {
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
         }
-        
+            
         tableView.deselectRow(at: indexPath, animated: true)
     }
-    
 
+    @IBAction func addItemPressed(_ sender: UIBarButtonItem) {
+        
+        var textField : UITextField?
+        
+        let alert = UIAlertController(title: "Adding new item", message: "", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Add item", style: .default, handler: {
+            (action) in
+            if let text = textField?.text {
+                self.todoListArray.append(text)
+                self.tableView.reloadData()
+                self.defaults.set(todoListArray, forKey: "itemsList")
+            } else {
+                print("Textfield was blank!")
+            }
+        }))
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Type in new item's name"
+            textField = alertTextField
+        }
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
 }
 
